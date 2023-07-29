e13f8a6b6b8ecc219e3e708961a486f8fee0d9df
Mahnaz Safavi <m.safavi69@gmail.com>
2023-07-29 11:51:10 +0330

    Added GitHub repo and footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Weather App</h1>
+      <div className="footer">
+        This project was coded by <a href='#' target='-blank'>Mahnaz Safavi</a> and is <a href='#'>open-sourced on GitHub</a>
+      </div>
       
     </div>
   );
