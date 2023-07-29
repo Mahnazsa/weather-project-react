17dd68c35a4ea698973fd3fe433a69a096a62b0a
Mahnaz Safavi <m.safavi69@gmail.com>
2023-07-29 18:07:56 +0330

    Installed Bootstarp and fixed the Emmet abbreviation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <div className="footer">
         This project was coded by <a href='https://roaring-sunburst-5cc45d.netlify.app/' target='-blank'>Mahnaz Safavi</a> and is <a href='https://github.com/Mahnazsa/weather-project-react'target='-blank'>open-sourced on GitHub</a>
       </div>
       
+    </div>
     </div>
   );
 }
