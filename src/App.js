c9f1a7d762a86eaa428fc8d969ad86943f8f166b
Mahnaz Safavi <m.safavi69@gmail.com>
2023-07-29 16:01:23 +0330

    Added personal page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <div className="footer">
-        This project was coded by <a href='#' target='-blank'>Mahnaz Safavi</a> and is <a href='#'>open-sourced on GitHub</a>
+        This project was coded by <a href='https://roaring-sunburst-5cc45d.netlify.app/' target='-blank'>Mahnaz Safavi</a> and is <a href='#https://github.com/Mahnazsa/weather-project-react'target='-blank'>open-sourced on GitHub</a>
       </div>
       
     </div>
